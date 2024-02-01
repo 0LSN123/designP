050ad43cddd39dc02169eef2bdf4e28f8c60cc21
lsn <nan1232021@163.com>
2024-02-01 20:33:02 +0800

    Factory

--- Factory/src/main/java/com/lsn/factory/FactoryMethod/BMW.java
+++ Factory/src/main/java/com/lsn/factory/FactoryMethod/BMW.java
@@ -1,5 +1,5 @@
 package com.lsn.factory.FactoryMethod;
 
 public interface  BMW {
-    public void showBMW() ;
+     void showBMW() ;
 }
--- Factory/src/main/java/com/lsn/factory/SimpleFactory/Product.java
+++ Factory/src/main/java/com/lsn/factory/SimpleFactory/Product.java
@@ -1,5 +1,5 @@
 package com.lsn.factory.SimpleFactory;
 
 public interface Product {
-    public void showProduct();
+     void showProduct();
 }
